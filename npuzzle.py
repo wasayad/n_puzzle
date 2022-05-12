469982d77522a033d83e17fc70b793d8e23f9583
akerdeka <kerdea@yahoo.fr>
2022-05-12 16:40:33 +0200

    pull

--- npuzzle.py
+++ npuzzle.py
@@ -23,6 +23,40 @@ class Npuzzle:
                 if i % array_size == 0:
                     self.map.append(flat_map[i:array_size + i])
             print(self.map)
+
+    def generateMatrix(self, n):
+            row1 = 0
+            col1 = 0
+            row2 = n
+            col2 = n
+            result = [ [0 for i in range(n)] for j in range(n)]
+            num = 1
+            while num<=n**2:
+               for i in range(col1,col2):
+                  result[row1][i] = num
+                  num+=1
+               if num > n**2:
+                  break
+               for i in range(row1+1,row2):
+                  result[i][col2-1] = num
+                  num+=1
+               if num > n**2:
+                  break
+               for i in range(col2-2,col1-1,-1):
+                  result[row2-1][i] = num
+                  num+=1
+               if num > n**2:
+                  break
+               for i in range(row2-2,row1,-1):
+                  result[i][col1] = num
+                  num+=1
+                  row1+=1
+                  row2-=1
+                  col1+=1
+                  col2-=1
+                  #print(result)
+            return result
             
 npuzzle = Npuzzle()
 npuzzle.map_parser()
+print(npuzzle.generateMatrix(4))
